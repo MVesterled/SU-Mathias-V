3843f81e776151baae895130e2876e135cb14e14
matmat1000 <mathias@vesterled.dk>
2024-04-15 21:39:14 +0200

    Init af git

---
+++ SU_Game_Mathias/main.cpp
@@ -0,0 +1,27 @@
+#include <QCoreApplication>
+#include <QtSql>
+#include <QSqlDatabase>
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+    QSqlDatabase db = QSqlDatabase::addDatabase("QMYSQL");
+    db.setHostName("localhost");
+    db.setDatabaseName("robot_worker");
+    db.setUserName("sammy");  // Change to username
+    db.setPassword("password");  // Change to password
+    db.open();
+
+    QSqlQuery query;
+
+    qDebug() << query.exec("SELECT * FROM task"); // employee er her navnet på en tabel, ikke et schema
+
+    qDebug() << "Number of rows:" << query.size();
+
+    qDebug() << "Finished";
+
+    std::cout << "say what" << std::endl;
+
+    return 1;
+}
